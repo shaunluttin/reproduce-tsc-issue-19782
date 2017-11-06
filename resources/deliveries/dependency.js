01f4aa57f44ed01d5b71fcdff0dc5211b755deb9
Shaun Luttin <admin@shaunluttin.com>
2017-11-06 15:27:19 -0800

    Simplify repro

--- resources/deliveries/dependency.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const validation_1 = require("@organization/validation");
-exports.default = () => {
-    // validation
-    const modelValidator = new validation_1.ModelValidator();
-    console.log(modelValidator);
-};
-//# sourceMappingURL=dependency.js.map
