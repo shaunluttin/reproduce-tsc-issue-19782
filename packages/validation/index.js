f00c188050229323e8f7e4f6af6f168e19148ab2
Shaun Luttin <admin@shaunluttin.com>
2017-11-06 15:29:04 -0800

    Remove *.js files

--- packages/validation/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const model_validator_1 = require("./model-validator");
-exports.ModelValidator = model_validator_1.default;
-//# sourceMappingURL=index.js.map
